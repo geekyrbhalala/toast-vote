223c32111a9efc3cfe9285d28ef97cffb8e1ac0b
ravibhalala11795 <geekyrbhalala@gmail.com>
2025-03-14 19:39:04 -0400

    - added route 53 module to s3 endpoint

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -9,3 +9,10 @@ module "frontend" {
   index_document = "home.html"
   error_document = "error.html"
 }
+
+
+module "map_domain_with_s3" {
+  source = "./modules/Route53"
+  domain_name = var.domain_name
+  s3_endpoint = module.frontend.website_url
+}
