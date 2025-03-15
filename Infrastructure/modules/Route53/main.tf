fd202706a844c8a4556c767c824501b9e744193c
ravibhalala11795 <geekyrbhalala@gmail.com>
2025-03-15 15:30:36 -0400

    update

--- Infrastructure/modules/Route53/main.tf
+++ Infrastructure/modules/Route53/main.tf
@@ -31,6 +31,8 @@ resource "aws_route53_record" "cert_validation" {
 resource "aws_acm_certificate_validation" "cert" {
   certificate_arn         = var.acm_cert_arn
   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+
+  depends_on = [ aws_route53_record.cert_validation ]
 }
 
 # Route 53 Record for CloudFront
